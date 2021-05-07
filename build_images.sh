34619d0dc29eb9180f538238b32435956fa689b8
Serhat Gülçiçek <serhat+github@equil.nl>
2021-05-07 10:07:10 +0200

    GitHub Action: Publish Docker images Updated the `build_images.sh` script to also do the push if requested.

--- build_images.sh
+++ build_images.sh
@@ -10,12 +10,23 @@ cd "${SCRIPT_DIR}"
 if [[ -z "${DOCKER_REGISTRY_URL:-}" ]]; then
   IMAGE_NAME='aerius-database-build'
 else
-  IMAGE_NAME="${DOCKER_REGISTRY_URL}/aerius-database-build"
+  IMAGE_NAME="${DOCKER_REGISTRY_URL}/database-build"
 fi
 
 # Loop through all generated Docker directories
+IMAGES_TO_PUSH=()
 while read DIRECTORY; do
   IMAGE_TAG=$(basename "${DIRECTORY}")
   echo '# Building: '"${IMAGE_TAG}"
   docker build --pull -t "${IMAGE_NAME}":"${IMAGE_TAG}" -f "${DIRECTORY}/Dockerfile" .
+
+  if [[ "${PUSH_IMAGES:-}" == 'true' ]]; then
+    IMAGES_TO_PUSH+=("${IMAGE_NAME}":"${IMAGE_TAG}")
+  fi
 done < <(find docker/ -maxdepth 1 -type d -name '*-*-*')
+
+# If there are images to push, do so
+for IMAGE_TO_PUSH in "${IMAGES_TO_PUSH[@]}"; do
+  echo '# Pushing image: '"${IMAGE_TO_PUSH}"
+  docker push "${IMAGE_TO_PUSH}"
+done
