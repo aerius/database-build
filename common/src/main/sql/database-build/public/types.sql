b3409d43a3870eadbd5c95f332942905bd0203f9
luitvm <38038189+eltee1@users.noreply.github.com>
2025-10-17 14:13:27 +0200

    added posnum, posreal datatypes to posnum, real to numeric: fraction datatype from real to numeric. function ae_linear_interpolate from real to numeric. posreal datatype retained for now, because it is still used in Calculator.

--- common/src/main/sql/database-build/public/types.sql
+++ common/src/main/sql/database-build/public/types.sql
@@ -17,9 +17,18 @@ CREATE DOMAIN posreal AS real
 
 
 /*
+ * posnum
+ * -------
+ * Numeric (decimal) value type which can only be positive or 0.
+ */
+CREATE DOMAIN posnum AS numeric
+	CHECK (VALUE >= 0::numeric);
+
+
+/*
  * fraction
  * --------
- * Real (decimal) value type between 0 and 1 (inclusive), specifying fractions.
+ * Numeric (decimal) value type between 0 and 1 (inclusive), specifying fractions.
  */
-CREATE DOMAIN fraction AS real
-	CHECK ((VALUE >= 0::real) AND (VALUE <= 1::real));
+CREATE DOMAIN fraction AS numeric
+	CHECK ((VALUE >= 0::numeric) AND (VALUE <= 1::numeric));
