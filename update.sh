3e28882965bc91f3bd9dc1a9344ddc88d12930e0
Bert Scholten <bscholten@dat.nl>
2026-05-07 15:59:39 +0200

    Add new postgresql version 18
    Postgres 15 is still an option technically, but with switching to a new snapshot it seems like a good moment to drop it.

--- update.sh
+++ update.sh
@@ -8,8 +8,8 @@ cd "${SCRIPT_DIR}"
 
 # Key is the PostgreSQL version and values are the PostGIS versions images are available for that PostgreSQL version.
 declare -A IMAGE_VERSIONS=(
-  [17]="3.5"
-  [15]="3.4"
+  [18]="3.6"
+  [17]="3.6"
 )
 
 # Read in current version of the script
